7179260cdb8cba5f26bc133a1ecb8ba3c090d649
RehasLiamsi <nsaher91@gmail.com>
2022-12-04 16:23:05 +0100

    Övning 1 and 2

---
+++ src/se/iths/twentytwo/övningsuppgifter/Arrayer.java
@@ -0,0 +1,36 @@
+package se.iths.twentytwo.övningsuppgifter;
+
+public class Arrayer {
+
+    public static void main(String[] args) {
+        quesTwo();
+    }
+
+    public static void quesOne() {
+        String[] colors = {"Gul", "Grön", "Orange", "Blå", "Grå"};
+        //REPLACED WITH ENCHANCED FOR LOOP
+        //for(int i = 0; i < colors.length; i++)
+        for (String color : colors) {
+            System.out.println(color);
+        }
+    }
+
+    public static void quesTwo() {
+        int[] numbers = {2, 3, 5, 8, 13, 21, 34};
+        int sum = 0;
+
+        /*for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }*/
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum = i % 2 == 0 ? sum + numbers[i] : sum - numbers[i];
+            /* //SIMPLIFIED VERSION OF ABOVE STATEMENT
+            if (i % 2 == 0)
+                sum += numbers[i];
+            else
+                sum -= numbers[i];*/
+        }
+        System.out.println(sum);
+    }
+}
