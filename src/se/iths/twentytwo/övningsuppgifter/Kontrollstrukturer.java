c48f684b6fd740d6e2c39a3b4b58656cf66f06c9
RehasLiamsi <nsaher91@gmail.com>
2022-12-04 14:03:29 +0100

    Övning 5

--- src/se/iths/twentytwo/övningsuppgifter/Kontrollstrukturer.java
+++ src/se/iths/twentytwo/övningsuppgifter/Kontrollstrukturer.java
@@ -8,7 +8,7 @@ public class Kontrollstrukturer {
 
     public static void main(String[] args) {
         Kontrollstrukturer v = new Kontrollstrukturer();
-        v.quesFour();
+        v.quesFive();
     }
 
     public void quesThree() {
@@ -42,5 +42,25 @@ public class Kontrollstrukturer {
             System.out.println("Talet är större än 100");
         else
             System.out.println("Talet är lika med 100");
+    }
+
+    public void quesFive() {
+        System.out.println("Skriv en månad");
+        String month = sc.nextLine();
+        month = month.toLowerCase();
+        switch (month) {
+            case "january" -> System.out.println("1");
+            case "february" -> System.out.println("2");
+            case "march" -> System.out.println("3");
+            case "april" -> System.out.println("4");
+            case "may" -> System.out.println("5");
+            case "june" -> System.out.println("6");
+            case "july" -> System.out.println("7");
+            case "august" -> System.out.println("8");
+            case "september" -> System.out.println("9");
+            case "october" -> System.out.println("10");
+            case "november" -> System.out.println("11");
+            case "december" -> System.out.println("12");
+        }
     }
 }
